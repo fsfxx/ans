a563c7ad70f7e730b487cdc84d5dec847102c61a
xiao <inxiao@outlook.com>
2017-10-23 21:55:28 +0800

    update

--- src/test/java/com/p/ans/service/OrderServiceTest.java
+++ src/test/java/com/p/ans/service/OrderServiceTest.java
@@ -25,38 +25,9 @@ import static org.mockito.Mockito.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class OrderServiceTest {
-    private OrderService orderService;
-    private UserRepository userRepository;
-    private GoodsRepository goodsRepository;
-    private OrderRepository orderRepository;
-    private long notExistUserId = -1;
-    private long notExistGoods = -1;
-    private long insufficientStockGoodsId = 1;
-    private long insufficientBalanceUserId = 1;
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        orderRepository = mock(OrderRepository.class);
-        when(orderRepository.findOne(notExistUserId))
-                .thenThrow(UserNotFoundException.class);
-        when(goodsRepository.findOne(notExistGoods))
-                .thenThrow(UserNotFoundException.class);
-        when(goodsRepository.findOne(insufficientBalanceUserId))
-                .thenThrow(InsufficientBalanceException.class);
-    }
-
     @Test
-    public void makeOrder() throws Exception {
+    public void test() throws Exception {
 
-    }
-
-    @Test
-    public void findAll() throws Exception {
     }
 
 }
